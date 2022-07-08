b7c4e483fab34c1babd8fb477a50e792fcc02cf6
chrisaddy <chris.william.addy@gmail.com>
2022-07-08 11:44:09 -0400

    update streamlit

--- examples/pages/forecasting.py
+++ examples/pages/forecasting.py
@@ -8,7 +8,7 @@ import plotly.express as px
 
 st.title("Univariate Time Series Data")
 
-slope = st.sidebar.slider("slope of trend", min_value=-100, max_value=100, step=0.1)
+slope = st.sidebar.slider("slope of trend", min_value=-100.0, max_value=100.0, step=0.1)
 num_samples = st.sidebar.slider("number of days", min_value = 10, max_value = 500, step=10)
 
 class LinearTrend(Simulator):
