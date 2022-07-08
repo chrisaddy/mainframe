5f66f5d4695525f94e3449e80a9dd61ea358cb98
chrisaddy <chris.william.addy@gmail.com>
2022-07-08 10:36:55 -0400

    update streamlit

--- examples/streamlit_app.py
+++ examples/streamlit_app.py
@@ -6,4 +6,8 @@ from mainframe.components import stochastic, deterministic
 st.title("Mainframe")
 
 
+st.slider("control mean", min_value = 0, max_value=100, step=1)
+st.slider("treatment mean", min_value = 0, max_value = 100, step=1)
+
+
 
