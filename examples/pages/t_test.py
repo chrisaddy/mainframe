cb7f73bed9e4c78ad43f87dd99e03aaac50398f5
chrisaddy <chris.william.addy@gmail.com>
2022-07-08 11:23:05 -0400

    update streamlit

--- examples/pages/t_test.py
+++ examples/pages/t_test.py
@@ -17,13 +17,24 @@ treatment_mean = st.sidebar.slider("treatment mean", min_value = 0, max_value = 
 treatment_variance = st.sidebar.slider("treatment variance", min_value = 1, max_value = 100, step=1)
 num_samples = st.sidebar.slider("number of samples to generate", min_value = 10, max_value = 10000, step=10)
 
-st.write("In `mainframe`, models are added to `Simulator` classes. These models are, at their core, pyro models.In essence, we define a prior probability distribution over which the simulator samples")
+st.write("In `mainframe`, models are added to `Simulator` classes. These models are, at their core, pyro models.In essence, we define a prior probability distribution over which the simulator samples.")
+
+st.write("In the case of a simple two-sample T-Test")
 
 st.markdown("""
 ```python
-def model(self):
-    control = stochastic("control", dist.Normal(*self.control))
-    treatment = stochastic("treatment", dist.Normal(*self.treatment))
+@dataclass
+class TwoSample(Simulator):
+    control: Tuple[float, float] = (0, 1)
+    treatment: Tuple[float, float] = (0, 1)
+
+    def model(self):
+        control = stochastic("control", dist.Normal(*self.control))
+        treatment = stochastic("treatment", dist.Normal(*self.treatment))
+
+    def model(self):
+        control = stochastic("control", dist.Normal(*self.control))
+        treatment = stochastic("treatment", dist.Normal(*self.treatment))
 ```
 """)
 
@@ -47,6 +58,11 @@ class TwoSample(Simulator):
 
 
 two_sample = TwoSample(control=(control_mean, control_variance), treatment=(treatment_mean, treatment_variance), num_samples=num_samples)
+st.markdown("""
+```python
+two_sample = TwoSample(control=(control_mean, control_variance), treatment=(treatment_mean, treatment_variance), num_samples=num_samples)
+```
+""")
 st.plotly_chart(two_sample.show())
 
 st.download_button("download simulated data", data=two_sample.dataframe.to_csv().encode("utf-8"), mime="text/csv")
