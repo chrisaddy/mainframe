f27e5c9e8a853d4a7d5632115792a85a3dc3d348
chrisaddy <chris.william.addy@gmail.com>
2022-07-08 11:14:40 -0400

    update streamlit

--- examples/main_page.py
+++ examples/main_page.py
@@ -11,10 +11,10 @@ def t_test():
     st.sidebar.markdown("# T-test stuff")
 
 
-page_names_to_funcs = {
-    "Home": main_page,
-    "T-Test": t_test,
-}
+# page_names_to_funcs = {
+#     "Home": main_page,
+#     "T-Test": t_test,
+# }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+# selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+# page_names_to_funcs[selected_page]()
