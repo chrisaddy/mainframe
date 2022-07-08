e386b25ad712f592bf6bc82af51edaee8c7aa613
chrisaddy <chris.william.addy@gmail.com>
2022-07-08 11:41:43 -0400

    update streamlit

--- examples/pages/forecasting.py
+++ examples/pages/forecasting.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import pyro.distributions as dist
 from mainframe import Simulator
 from mainframe.components import stochastic, deterministic
 import plotly.express as px
@@ -7,17 +8,17 @@ import plotly.express as px
 
 st.title("Univariate Time Series Data")
 
-slope = st.sidebar("slope of trend", min_value=-100, max_value=100, default=0, step=0.1)
+slope = st.sidebar("slope of trend", min_value=-100, max_value=100, step=0.1)
 num_samples = st.sidebar.slider("number of days", min_value = 10, max_value = 500, step=10, default=365)
 
 class LinearTrend(Simulator):
-    num_days = 365
+    num_dates = 365
     slope = 5
 
     def model(self):
 
         day = stochastic("_day", dist.Uniform(0, 1))
-        day = deterministic("day", (day * num_dates).round())
+        day = deterministic("day", (day * self.num_dates).round())
 
         brownian = stochastic("brownian", dist.Normal(0, 2))
         trend = deterministic("trend", self.slope * day + brownian)
